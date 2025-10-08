2f8d8eace9dd9c155ab37807f1879ca3bd7c3f1b
LETI-122653 <sheao@iscte-iul.pt>
2025-10-09 00:24:33 +0100

    Criei a estura para gerar pdf

---
+++ src/main/java/com/example/examplefeature/ui/PdfService.java
@@ -0,0 +1,12 @@
+package com.example.examplefeature.ui;
+
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("gerar-pdf")
+@PageTitle("Gerar PDF")
+@Menu(order = 1, icon = "vaadin:file-text", title = "Gerar PDF")
+
+public class PdfService {
+}
